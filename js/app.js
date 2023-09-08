f32a91d505e3f7cb5c7ce6e7de8758fe2e3df013
José Poleo <jose.poleo.2a@gmail.com>
2023-09-08 19:24:53 -0400

    FEAT:Menu Mobile

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ function main(){
     let navEmail = document.querySelector('.navbar-email');
     navEmail.addEventListener('click',toggleNavDesktop);
 
-
+    let menuHamburguesa = document.querySelector('.menu');
+    menuHamburguesa.addEventListener('click',toggleNavMobile)
 }
 
 
@@ -11,6 +12,13 @@ function toggleNavDesktop(e) {
     
     let navDesktop = document.querySelector('.desktop-menu');
     navDesktop.classList.toggle('inactive');
+
+}
+
+function toggleNavMobile(e) {
+    
+    let navMobile = document.querySelector('.mobile-menu');
+    navMobile.classList.toggle('inactive');
 
 }
 
