421f719a20da1de659f45a78dadec0299e362f4b
José Poleo <jose.poleo.2a@gmail.com>
2023-09-08 20:12:17 -0400

    FEAT:Carrito de Compras

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ function main(){
 
     let menuHamburguesa = document.querySelector('.menu');
     menuHamburguesa.addEventListener('click',toggleNavMobile)
+
+    let carrito = document.querySelector('.navbar-shopping-cart');
+    carrito.addEventListener('click',toggleCarritoCompras);
 }
 
 
@@ -12,6 +15,11 @@ function toggleNavDesktop(e) {
     
     let navDesktop = document.querySelector('.desktop-menu');
     navDesktop.classList.toggle('inactive');
+    let carritoCompras = document.querySelector('.product-detail');
+
+    if(!carritoCompras.classList.contains('inactive')){
+        carritoCompras.classList.add('inactive');
+    }
 
 }
 
@@ -19,6 +27,28 @@ function toggleNavMobile(e) {
     
     let navMobile = document.querySelector('.mobile-menu');
     navMobile.classList.toggle('inactive');
+    let carritoCompras = document.querySelector('.product-detail');
+
+    if(!carritoCompras.classList.contains('inactive')){
+        carritoCompras.classList.add('inactive');
+    }
+
+}
+
+function toggleCarritoCompras(e){
+
+    let carritoCompras = document.querySelector('.product-detail');
+    carritoCompras.classList.toggle('inactive');
+    let navMobile = document.querySelector('.mobile-menu');
+    let navDesktop = document.querySelector('.desktop-menu');
+
+    if(!navMobile.classList.contains('inactive')){
+        navMobile.classList.add('inactive');
+    }
+
+    if(!navDesktop.classList.contains('inactive')){
+        navDesktop.classList.add('inactive');
+    }
 
 }
 
